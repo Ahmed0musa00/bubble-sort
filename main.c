2cd92d07647763a9c36016e9bc5c32fc12314326
Ahmed0musa00 <musaa8055@gmail.com>
2019-07-31 23:59:10 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 void bubble_sort(int arr[],int n);
+void bubbleSort (int res []);
 
-int main()
+int main(void)
 {
     int arr[6]={5,7,66,8,-5,2};
-    bubble_sort(arr, 6);
+    bubbleSort(arr);
     int i;
     for(i=0;i<6;i++)
     printf("%d\t",arr[i]);
 
     return 0;
+}
+
+
+void bubbleSort (int res [])
+{
+    int temp;
+    int i;
+    int j;
+    for(i=1;i<6;i++)
+    {
+        for(j=0;j<6-1;j++)
+        {
+            if(res[j]>res[j+1])
+            {
+               temp=res[j];
+               res[j]=res[j+1];
+               res[j+1]=temp;
+            }
+        }
+    }
 }
 
 
